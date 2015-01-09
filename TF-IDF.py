b4ca6333c83812cd29f372ea352d609ae077cc13
Mateusz Staruchowicz <starooch@gmail.com>
2015-01-09 21:14:52 +0100

    Add IDF method implementation

--- TF-IDF.py
+++ TF-IDF.py
@@ -1,6 +1,7 @@
 __author__ = 'Mateusz'
 
 from collections import Counter
+from math import log
 
 firstDoc = 'The game of life is a game of everlasting learning.'
 secondDoc = 'The unexamined life is not worth living.'
@@ -45,3 +46,35 @@ for i in (firstDoc, secondDoc, thirdDoc):
 
 # IDF
 
+def inverseDocumentFrequency(word, docs):
+
+    numOfDocs = len(docs)
+    numDocWithWord = 0
+
+    for doc in docs:
+        if word.lower() in doc.lower().split():
+            numDocWithWord = numDocWithWord + 1
+
+    if numDocWithWord > 0:
+        return 1.0 + log(float(numOfDocs) / numDocWithWord)
+    else:
+        return 1.0
+
+def idfForDoc(docs):
+
+    arrayOfResults = []
+    for doc in docs:
+
+        docSplit = doc.lower().split()
+        docDict = Counter(docSplit)
+
+        for key, value in docDict.iteritems():
+            value = inverseDocumentFrequency(key, docs)
+            docDict[key] = value
+
+        arrayOfResults.append(docDict)
+    return arrayOfResults
+
+a = (firstDoc, secondDoc, thirdDoc)
+i = idfForDoc(a)
+print(i)
